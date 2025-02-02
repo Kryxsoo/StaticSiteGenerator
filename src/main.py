9e50668df347e527d546e9865a822f2ef8724e4f
Mathew Charm <mattcharmz@gmail.com>
2025-02-01 17:18:52 -0800

    TextNodes

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextType, TextNode
+
+def main():
+  test = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+  print(test)
+
+if __name__ == "__main__":
+  main()
